1e51282944ab0701aab78d44db64fa29c4392e62
gaustin15 <35272713+gaustin15@users.noreply.github.com>
2022-08-08 03:07:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     description="Source-tracking for Contamination Removal in microBiomes (SCRuB)",
     license="BSD-3-Clause",
     url="https://github.com/korem-lab/SCRuB",
+    install_requires=['pyreadr'],
     entry_points={
         'qiime2.plugins': ['q2-SCRuB=q2_SCRuB.plugin_setup:plugin']
     },
