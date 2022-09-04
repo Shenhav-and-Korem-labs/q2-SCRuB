049902aa0788b5a99cb20f431fc10faf54dcc78e
George Austin <george@georges-mbp.myfiosgateway.com>
2022-09-03 22:22:53 -0400

    new approach to build R-env

--- q2_SCRuB/assets/run_SCRuB.R
+++ q2_SCRuB/assets/run_SCRuB.R
@@ -62,10 +62,10 @@ for(fn in c(out.path)) {
 }
 
 ## LOAD LIBRARIES ###
-suppressWarnings(library(SCRuB))
-suppressWarnings(library(stringr))
-suppressWarnings(library(dplyr))
-suppressWarnings(library(rlang))
+suppressMessages({library(SCRuB)}, quitely=TRUE)
+suppressMessages({library(stringr)}, quitely=TRUE)
+suppressMessages({library(dplyr)}, quitely=TRUE)
+suppressMessages({library(rlang)}, quitely=TRUE)
 cat("SCRuB:", as.character(packageVersion("SCRuB")), "\n")
 cat("1) Loading datas\n")
   samples <- read.csv(inp.samps, row.names=1) %>% as.matrix()
